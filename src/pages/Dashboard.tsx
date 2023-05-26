05df5cef9f0644631a1ad0c6cfbc393a5fa3d68a
Franklin Castillo <franklincastillo18@gmail.com>
2023-05-25 20:40:16 -0400

    ISSUE-21:Create initial project structure and path Adding routes and initial components

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ContainerLayout from '../components/ContainerLayout/ContainerLayout';
+
+const Dashboard: React.FC = () => {
+  return <ContainerLayout>Dashboard Page</ContainerLayout>;
+};
+
+export default Dashboard;
---
+++ src/pages/User.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContainerLayout from '../components/ContainerLayout/ContainerLayout';
+
+const User: React.FC = () => {
+  return (
+    <ContainerLayout>
+      <div>User Page</div>
+    </ContainerLayout>
+  );
+};
+
+export default User;
