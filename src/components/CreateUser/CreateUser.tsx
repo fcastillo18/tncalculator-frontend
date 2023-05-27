c66b7dffd5d1d070fdac5452fc4becf1ccb408d6
Franklin Castillo <franklincastillo18@gmail.com>
2023-05-26 21:03:12 -0400

    ISSUE-36: create user page
    Added two new components.
    -CreateUser: which hold the inputs and logic to create a component
    -UserRecords: to display all users from the database

---
+++ src/components/CreateUser/CreateUser.tsx
@@ -0,0 +1,127 @@
+import {
+  Button,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  SelectChangeEvent,
+  Grid,
+  Typography,
+} from '@mui/material';
+import { useState } from 'react';
+import { ERole } from '../../types/Enums';
+
+const CreateUser: React.FC = () => {
+  const [username, setUsername] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [balance, setBalance] = useState<number>(0);
+  const [role, setRole] = useState<ERole>(ERole.USER);
+
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleBalanceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setBalance(Number(event.target.value));
+  };
+
+  const handleRoleChange = (event: SelectChangeEvent<ERole>) => {
+    setRole(event.target.value as ERole);
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const response = await fetch('/api/user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+        balance,
+        role,
+      }),
+    });
+
+    const data = await response.json();
+
+    alert(`User created with ID: ${data.id}`);
+  };
+
+  return (
+    <form noValidate autoComplete="off">
+      <Typography variant="h5" component="h2" sx={{ mt: 6, mb: 4 }}>
+        Create User
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <TextField
+            label="Username"
+            value={username}
+            onChange={handleUsernameChange}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            label="Email"
+            value={email}
+            onChange={handleEmailChange}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            label="Password"
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            label="Balance"
+            type="number"
+            value={balance || ''}
+            onChange={handleBalanceChange}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <FormControl>
+            <InputLabel id="role-label">Role</InputLabel>
+            <Select
+              labelId="role-label"
+              value={role}
+              onChange={handleRoleChange}
+            >
+              <MenuItem value={'admin'}>Admin</MenuItem>
+              <MenuItem value={'user'}>User</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Create User
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
+
+export default CreateUser;
--- src/mocks/mocks.ts
+++ src/mocks/mocks.ts
@@ -1,4 +1,5 @@
 import { OperationResult, Record } from '../types/RecordTypes';
+import { Role, User } from '../types/UserTypes';
 
 // TODO this sample data should be used in storybook, after implementation
 export const sampleOperationResult: OperationResult = {
@@ -9,7 +10,7 @@ export const sampleOperationResult: OperationResult = {
   userRemainingBalance: 100,
 };
 // TODO this sample data should be used in storybook, after implementation
-export const sampleRecords: Record[] = [
+export const sampleOperationRecords: Record[] = [
   {
     id: 1,
     operation: 'ADDITION',
@@ -63,5 +64,51 @@ export const sampleRecords: Record[] = [
     operationResponse: 'SUCCESS',
     date: new Date(),
     isDeleted: false,
+  },
+];
+
+const adminRoleSample: Role[] = [{ id: 1, name: 'admin' }];
+const userRoleSample: Role[] = [{ id: 2, name: 'user' }];
+
+export const sampleUserRecords: User[] = [
+  {
+    id: 1,
+    username: 'admin',
+    password: 'admin',
+    email: 'admin@example.com',
+    roles: adminRoleSample,
+    balance: 100,
+  },
+  {
+    id: 2,
+    username: 'user2',
+    password: 'pass2',
+    email: 'user2@example.com',
+    roles: userRoleSample,
+    balance: 500,
+  },
+  {
+    id: 3,
+    username: 'admin2',
+    password: 'admin2',
+    email: 'admin2@example.com',
+    roles: adminRoleSample,
+    balance: 90,
+  },
+  {
+    id: 4,
+    username: 'user4',
+    password: 'pass4',
+    email: 'user4@example.com',
+    roles: userRoleSample,
+    balance: 250,
+  },
+  {
+    id: 5,
+    username: 'user5',
+    password: 'pass5',
+    email: 'user5@example.com',
+    roles: userRoleSample,
+    balance: 50,
   },
 ];
