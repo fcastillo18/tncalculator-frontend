0d64842ba1c7f929be7815a0687978a7657e8d96
Franklin Castillo <franklincastillo18@gmail.com>
2023-05-29 05:49:35 -0400

    ISSUE-32: Integrate api to operation records page
    Some other fixes to type and storybook were applied.

--- src/types/RecordTypes.ts
+++ src/types/RecordTypes.ts
@@ -1,11 +1,28 @@
+import { User } from './UserTypes';
+
 export type Record = {
   id: number;
-  operation: string;
-  user: string;
+  operation: Operation;
+  user: User;
   amount: number;
   operationResponse: string;
   date: Date;
-  isDeleted: boolean;
+  deleted: boolean;
+};
+
+export enum OperationType {
+  ADDITION = 'ADDITION',
+  SUBTRACTION = 'SUBTRACTION',
+  MULTIPLICATION = 'MULTIPLICATION',
+  DIVISION = 'DIVISION',
+  SQUARE_ROOT = 'SQUARE_ROOT',
+  RANDOM_STRING = 'RANDOM_STRING',
+}
+
+export type Operation = {
+  id: number;
+  type: OperationType;
+  cost: number;
 };
 
 export interface OperationData {
