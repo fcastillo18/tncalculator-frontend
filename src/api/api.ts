b50e56cd15d9e22ee97055947e6f01da07b00bd9
Franklin Castillo <franklincastillo18@gmail.com>
2023-06-02 08:42:24 -0400

    fix: env var

--- src/api/api.ts
+++ src/api/api.ts
@@ -13,8 +13,8 @@ import {
 import { SignupRequest, User } from '../types/UserTypes';
 
 const url =
-  import.meta.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api/v1';
-// 'http://ec2-3-89-20-5.compute-1.amazonaws.com/api/v1';
+  import.meta.env.REACT_APP_API_BASE_URL ||
+  'http://ec2-3-89-20-5.compute-1.amazonaws.com/api/v1';
 const api = axios.create({
   baseURL: url,
 });
